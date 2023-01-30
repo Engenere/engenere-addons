7060aa308522ceb81a9b1a5f6d10a3a91175f27b
Felipe <felipe@engenere.one>
2023-01-30 15:02:36 -0300

    [ADD] sale_order_general_client_order_ref: new module

---
+++ sale_order_general_client_order_ref/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2023-Today - Engenere (<https://engenere.one>).
+# @author Felipe Motter Pereira <felipe@engenere.one>
+
+{
+    "name": "Sale Order General Cliente Order Ref",
+    "summary": "Take the General Client Order Ref on Sale Order to Partner Order in Sale Order Line.",
+    "version": "14.0.0.0.0",
+    "development_status": "Alpha",
+    "category": "Sales",
+    "website": "https://github.com/Engenere/engenere-addons",
+    "author": "Engenere",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["sale", "l10n_br_fiscal"],
+}
