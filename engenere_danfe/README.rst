d48fccca326ecbe8040fadcad5cb928a6167a258
Felipe Motter <felipe@engenere.one>
2023-06-06 22:22:49 -0300

    [ADD] engenere_danfe: init module

---
+++ engenere_danfe/README.rst
@@ -0,0 +1,23 @@
+==============
+Engenere Danfe
+==============
+
+Generate a new danfe layout compatible with OCA/l10n_brazil
+
+Configuration
+=============
+
+To configure this module, you need to:
+
+#. Go to ...
+
+Usage
+=====
+
+To use this module, you need to:
+
+#. Go to ...
+
+
+Changelog
+=========
---
+++ engenere_danfe/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Engenere.one
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Engenere Danfe',
+    'description': """
+        Generate a new danfe layout compatible with OCA/l10n_brazil""",
+    'version': '14.0.0.0.0',
+    'license': 'AGPL-3',
+    'author': 'Engenere.one',
+    'website': 'https://engenere.one',
+    'depends': [
+        "l10n_br_account",
+    ],
+    'data': [
+        "reports/danfe_report.xml"
+    ],
+    'demo': [
+    ],
+}
