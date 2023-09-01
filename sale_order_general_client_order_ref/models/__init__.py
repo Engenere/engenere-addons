0e027843c45cfbb4358dddeec16a533e2474bb9a
Felipe Motter <felipe@engenere.one>
2023-08-31 21:24:57 -0300

    [REF] sale_order_general_client_order_ref: simplify code

--- sale_order_general_client_order_ref/models/__init__.py
+++ sale_order_general_client_order_ref/models/__init__.py
@@ -1,5 +1,4 @@
 # Copyright (C) 2023-Today - Engenere (<https://engenere.one>).
 # @author Felipe Motter Pereira <felipe@engenere.one>
 
-from . import sale_order
 from . import sale_order_line
--- sale_order_general_client_order_ref/models/sale_order_line.py
+++ sale_order_general_client_order_ref/models/sale_order_line.py
@@ -7,23 +7,9 @@ from odoo import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    partner_order = fields.Text(
-        compute="_compute_partner_order",
-        store=True,
-        readonly=False,
-    )
+    partner_order = fields.Char(compute='_compute_partner_order', store=True, readonly=False)
 
-    @api.model
-    def create(self, vals):
-        if "partner_order" not in vals and "order_id" in vals:
-            sale_order = self.env["sale.order"].browse(vals["order_id"])
-            if sale_order.client_order_ref:
-                vals["partner_order"] = sale_order.client_order_ref
-        return super().create(vals)
-
-    @api.depends("order_id", "order_id.client_order_ref")
+    @api.depends('order_id','order_id.client_order_ref')
     def _compute_partner_order(self):
-        # if hasattr(super(), "_compute_partner_order"):
-        #     super()._compute_partner_order()
-        for line in self:
-            line.partner_order = line.order_id.client_order_ref
+        for record in self:
+            record.partner_order = record.order_id.client_order_ref if record.order_id else False
