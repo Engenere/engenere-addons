f2866968b2ff9c7d190797239bc0773eab43584d
Antônio Neto <neto@engenere.one>
2023-09-04 22:13:56 -0300

    [ADD] l10n_br_account_usability

---
+++ l10n_br_account_usability/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2023-Today - Engenere (<https://engenere.one>).
+# @author Antônio S. Pereira Neto <neto@engenere.one>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Usabilidades para Faturamento e Contabilidade",
+    "summary": "Usabilidades para Faturamento e Contabilidade",
+    "category": "Localisation",
+    "license": "AGPL-3",
+    "author": "Engenere",
+    "maintainers": ["antoniospneto", "felipemotter"],
+    "website": "https://github.com/Engenere/engenere-addons",
+    "version": "14.0.0.0.0",
+    "development_status": "Beta",
+    "depends": [
+        "l10n_br_account",
+    ],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "installable": True,
+}
