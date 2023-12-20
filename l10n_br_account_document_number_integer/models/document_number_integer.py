c5cdbc54130b483664fbf17e241afe00647a5d0b
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2023-12-20 10:41:23 -0300

    [ADD] l10n_br_account_document_number_integer

---
+++ l10n_br_account_document_number_integer/models/document_number_integer.py
@@ -0,0 +1,11 @@
+from odoo import models, fields, api
+
+class DocumentNumberInteger(models.Model):
+    _inherit = "account.move"
+
+    document_number_integer = fields.Integer(string='document_number_integer', compute='_compute_document_member_integer',store=True)
+
+    @api.depends('document_number')
+    def _compute_document_member_integer(self):
+        for record in self:
+            record.document_number_integer = int(record.document_number)
