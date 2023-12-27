a73a1a9b88fb701931493544d45523624031fbd6
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2023-12-27 12:36:54 -0300

    [ADD] eng_account_reconcile_usability

---
+++ eng_account_reconcile_usability/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Visibilidade dos campos debito e credito correspodente",
+    "summary": "Visibilidade dos campos debito correspodente e credito correspodente.",
+    "license": "AGPL-3",
+    "author": "Engenere",
+    "maintainers": ["cristianomafrajunior"],
+    "website": "https://github.com/Engenere/engenere-addons",
+    "version": "14.0.1.0.0",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_move_line.xml",
+    ],
+    "installable": True,
+}
