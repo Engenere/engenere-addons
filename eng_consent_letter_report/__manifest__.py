b2b5eebae057ebaaa4ab15582f9c02bffe07506e
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2024-01-02 12:18:12 -0300

    [ADD] eng_consent_letter_report

---
+++ eng_consent_letter_report/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Relatorio de Carta de Anuência",
+    "summary": "Relatorio em PDF de carta de Anuência.",
+    "license": "AGPL-3",
+    "author": "Engenere",
+    "maintainers": ["cristianomafrajunior"],
+    "website": "https://github.com/Engenere/engenere-addons",
+    "version": "14.0.1.0.0",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "report/account_move_line_template.xml",
+        "report/account_move_line_report.xml",
+    ],
+    "installable": True,
+}
