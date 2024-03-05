1ef88e6b701cff55444961a516516403bf543f71
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2024-03-05 09:27:19 -0300

    [ADD] eng_account_show_reconcile_button: visibility to the open_reconcile_view field

---
+++ eng_account_show_reconcile_button/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Visibility of the Reconciled Entries button.",
+    "summary": "Visibility of the Reconciled Entries button.",
+    "license": "AGPL-3",
+    "author": "Engenere",
+    "maintainers": ["cristianomafrajunior"],
+    "website": "https://github.com/Engenere/engenere-addons",
+    "version": "14.0.1.0.0",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "installable": True,
+}
