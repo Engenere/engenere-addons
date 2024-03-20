124af3c64e5781269ef19b3f751711b6e235d24b
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2024-03-20 15:26:16 -0300

    [REF] sale_order_general_client_order_ref: add pre-commit

--- sale_order_general_client_order_ref/models/sale_order_line.py
+++ sale_order_general_client_order_ref/models/sale_order_line.py
@@ -7,9 +7,13 @@ from odoo import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    partner_order = fields.Char(compute='_compute_partner_order', store=True, readonly=False)
+    partner_order = fields.Char(
+        compute="_compute_partner_order", store=True, readonly=False
+    )
 
-    @api.depends('order_id','order_id.client_order_ref')
+    @api.depends("order_id", "order_id.client_order_ref")
     def _compute_partner_order(self):
         for record in self:
-            record.partner_order = record.order_id.client_order_ref if record.order_id else False
+            record.partner_order = (
+                record.order_id.client_order_ref if record.order_id else False
+            )
