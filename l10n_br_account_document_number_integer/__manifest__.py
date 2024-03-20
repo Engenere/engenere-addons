b2936f78530948feed5abd1decad57c25e791857
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2024-03-20 15:17:40 -0300

    [REF] l10n_br_account_document_number_integer: add pre-commit

--- l10n_br_account_document_number_integer/__manifest__.py
+++ l10n_br_account_document_number_integer/__manifest__.py
@@ -4,7 +4,7 @@
     "license": "AGPL-3",
     "author": "Engenere",
     "maintainers": ["cristianomafrajunior"],
-    "website": "https://github.com/Engenere/engenere-addons",
+    "website": "https://engenere.one",
     "version": "14.0.1.0.0",
     "development_status": "Beta",
     "depends": [
@@ -14,6 +14,4 @@
         "views/document_number_views.xml",
     ],
     "installable": True,
-
-
 }
