3cf324f81601acbe10422488e58cc6c5a71e30d6
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2024-03-20 15:22:50 -0300

    [REF] l10n_br_base_allows_duplicate_cnpj_ie: add pre-commit

--- l10n_br_base_allows_duplicate_cnpj_ie/__manifest__.py
+++ l10n_br_base_allows_duplicate_cnpj_ie/__manifest__.py
@@ -2,12 +2,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Parceiros com CNPJ/IE duplicados',
-    'version': '14.0.0.0.0',
-    'license': 'AGPL-3',
-    'author': 'Engenere.one',
-    'website': 'https://engenere.one',
-    'depends': [
+    "name": "Parceiros com CNPJ/IE duplicados",
+    "version": "14.0.0.0.0",
+    "license": "AGPL-3",
+    "author": "Engenere",
+    "maintainers": ["felipemotter"],
+    "website": "https://engenere.one",
+    "depends": [
         "l10n_br_base",
     ],
 }
