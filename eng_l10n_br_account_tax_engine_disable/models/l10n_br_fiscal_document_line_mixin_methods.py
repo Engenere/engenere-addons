e8ec642e70743366b24d2d0f7159ac8ee4d970f6
Felipe Motter Pereira <felipe@engenere.one>
2024-08-07 14:53:33 -0300

    [FIX] eng_l10n_br_account_tax_engine_disable: amount_tax_included when disabled

--- eng_l10n_br_account_tax_engine_disable/models/l10n_br_fiscal_document_line_mixin_methods.py
+++ eng_l10n_br_account_tax_engine_disable/models/l10n_br_fiscal_document_line_mixin_methods.py
@@ -30,3 +30,31 @@ class FiscalDocumentLineMixinMethods(models.AbstractModel):
         return (
             hasattr(self, "document_id") and self.document_id.fiscal_tax_engine_disabled
         )
+
+    def _update_taxes(self):
+        super()._update_taxes()
+        if self._is_fiscal_tax_engine_disabled():
+            self._update_taxes_when_disabled()
+
+    def _update_taxes_when_disabled(self):
+        tax_groups = self.env["l10n_br_fiscal.tax.group"].search([])
+
+        for line in self:
+            amount_tax_included = 0
+            amount_tax_not_included = 0
+            amount_tax_withholding = 0
+            for group in tax_groups:
+                tax_domain_value = group.tax_domain + "_value"
+
+                if hasattr(line, tax_domain_value):
+                    tax_value = getattr(line, tax_domain_value)
+                    if group.tax_include:
+                        amount_tax_included += tax_value
+                    else:
+                        amount_tax_not_included += tax_value
+                    if group.tax_withholding:
+                        amount_tax_withholding += tax_value
+
+            line.amount_tax_included = amount_tax_included
+            line.amount_tax_not_included = amount_tax_not_included
+            line.amount_tax_withholding = amount_tax_withholding
