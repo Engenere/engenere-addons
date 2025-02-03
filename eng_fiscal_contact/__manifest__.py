8040334750b5fd534cff6176a1f2a9bcfe1befa7
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2025-02-03 16:39:51 -0300

    [MIG] eng_fiscal_contact: Migration to 16.0

--- eng_fiscal_contact/__manifest__.py
+++ eng_fiscal_contact/__manifest__.py
@@ -8,10 +8,11 @@
     "license": "AGPL-3",
     "author": "Engenere",
     "maintainers": ["cristianomafrajunior"],
-    "website": "https://engenere.one",
-    "version": "14.0.1.0.0",
+    "website": "https://github.com/Engenere/engenere-addons",
+    "version": "16.0.1.0.0",
     "depends": [
         "l10n_br_base",
+        "contacts",
     ],
     "data": [
         "views/res_partner_views.xml",
---
+++ eng_fiscal_contact/readme/DESCRIPTION.md
@@ -0,0 +1,3 @@
+## Eng Fiscal Contact
+
+O módulo permite que os usuários adicionem indivíduos-chave para questões fiscais, agilizando a comunicação sobre alterações de status de documentos. Esta adição simplifica o gerenciamento de contatos.
