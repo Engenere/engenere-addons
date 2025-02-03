271906bd3acc264a3767dfb90809848a8aa273ad
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2025-02-03 15:16:15 -0300

    [MIG] eng_consent_letter_report: Migration to 16.0

--- eng_consent_letter_report/__manifest__.py
+++ eng_consent_letter_report/__manifest__.py
@@ -1,12 +1,12 @@
 {
-    "name": "Relatorio de Carta de Anuência",
-    "summary": "Relatorio em PDF de carta de Anuência.",
+    "name": "Relatório de Carta de Anuência",
+    "summary": "Relatório em PDF de carta de Anuência.",
     "license": "AGPL-3",
     "author": "Engenere",
     "maintainers": ["cristianomafrajunior"],
-    "website": "https://engenere.one",
-    "version": "14.0.1.0.0",
-    "depends": ["account"],
+    "website": "https://github.com/Engenere/engenere-addons",
+    "version": "16.0.1.0.0",
+    "depends": ["l10n_br_account", "account_reconcile_oca"],
     "data": [
         "report/account_move_line_template.xml",
         "report/account_move_line_report.xml",
---
+++ eng_consent_letter_report/readme/DESCRIPTION.md
@@ -0,0 +1,3 @@
+## Relatório de Carta de Anuência
+
+Este relatório gera a Carta de Anuência, um documento formal utilizado para confirmar a concordância com a quitação de uma obrigação ou pendência. Ele contém informações detalhadas do Duplicada, Valor, Vencimento e outras informações relevantes para a Carta de Anuência.
