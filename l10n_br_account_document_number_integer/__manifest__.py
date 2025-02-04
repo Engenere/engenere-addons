369a7002a3f581696cfb8a1a0b5e337396e622e0
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2025-02-04 09:57:02 -0300

    [MIG] l10n_br_account_document_number_integer: Migration to 16.0

--- l10n_br_account_document_number_integer/__manifest__.py
+++ l10n_br_account_document_number_integer/__manifest__.py
@@ -1,11 +1,11 @@
 {
-    "name": "Alteração Numero do Documento Inteiro",
-    "summary": "Criação do campo int para melhor organização na busca",
+    "name": "Change Document Number to Integer",
+    "summary": "Creation of the int field for better search organization",
     "license": "AGPL-3",
     "author": "Engenere",
     "maintainers": ["cristianomafrajunior"],
-    "website": "https://engenere.one",
-    "version": "14.0.1.0.0",
+    "website": "https://github.com/Engenere/engenere-addons",
+    "version": "16.0.1.0.0",
     "development_status": "Beta",
     "depends": [
         "l10n_br_account",
