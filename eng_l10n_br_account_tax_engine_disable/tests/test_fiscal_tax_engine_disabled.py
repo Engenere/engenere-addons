d2894021696851588b06678996afae3d25549aad
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2025-03-20 11:07:37 -0300

    [REF] eng_l10n_br_account_tax_engine_disable: setUpClass

--- eng_l10n_br_account_tax_engine_disable/tests/test_fiscal_tax_engine_disabled.py
+++ eng_l10n_br_account_tax_engine_disable/tests/test_fiscal_tax_engine_disabled.py
@@ -5,40 +5,41 @@ from odoo.tests.common import TransactionCase
 
 
 class TestFiscalDocumentLineMixinMethods(TransactionCase):
-    def setUp(self):
-        super().setUp()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
 
-        self.fiscal_taxes = (
-            self.env.ref("l10n_br_fiscal.tax_icms_7")
-            + self.env.ref("l10n_br_fiscal.tax_ipi_15")
-            + self.env.ref("l10n_br_fiscal.tax_pis_0_65")
-            + self.env.ref("l10n_br_fiscal.tax_cofins_3")
+        cls.fiscal_taxes = (
+            cls.env.ref("l10n_br_fiscal.tax_icms_7")
+            + cls.env.ref("l10n_br_fiscal.tax_ipi_15")
+            + cls.env.ref("l10n_br_fiscal.tax_pis_0_65")
+            + cls.env.ref("l10n_br_fiscal.tax_cofins_3")
         )
 
-        self.fiscal_doc = self.env["l10n_br_fiscal.document"].create(
+        cls.fiscal_doc = cls.env["l10n_br_fiscal.document"].create(
             {
-                "fiscal_operation_id": self.env.ref("l10n_br_fiscal.fo_compras").id,
-                "document_type_id": self.env.ref("l10n_br_fiscal.document_55").id,
+                "fiscal_operation_id": cls.env.ref("l10n_br_fiscal.fo_compras").id,
+                "document_type_id": cls.env.ref("l10n_br_fiscal.document_55").id,
                 "document_serie": 1,
                 "document_number": 123,
                 "issuer": "partner",
-                "partner_id": self.env["res.partner"]
+                "partner_id": cls.env["res.partner"]
                 .create({"name": "Fornecedor X"})
                 .id,
                 "fiscal_operation_type": "in",
             }
         )
 
-        self.fiscal_line = self.env["l10n_br_fiscal.document.line"].create(
+        cls.fiscal_line = cls.env["l10n_br_fiscal.document.line"].create(
             {
-                "document_id": self.fiscal_doc.id,
+                "document_id": cls.fiscal_doc.id,
                 "name": "Compra Teste",
-                "product_id": self.env["product.product"]
+                "product_id": cls.env["product.product"]
                 .create({"name": "Produto Teste"})
                 .id,
                 "fiscal_operation_type": "in",
-                "fiscal_operation_id": self.env.ref("l10n_br_fiscal.fo_compras").id,
-                "fiscal_operation_line_id": self.env.ref(
+                "fiscal_operation_id": cls.env.ref("l10n_br_fiscal.fo_compras").id,
+                "fiscal_operation_line_id": cls.env.ref(
                     "l10n_br_fiscal.fo_compras_compras"
                 ).id,
                 "icms_value": 10.0,
