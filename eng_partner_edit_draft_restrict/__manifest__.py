a294cad2f5565d8c42f8d8e5299d77c7b06330a2
Felipe Motter Pereira <felipe@engenere.one>
2025-06-24 13:12:21 -0300

    [ADD] eng_partner_edit_draft_restrict: init module

---
+++ eng_partner_edit_draft_restrict/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Engenere.one
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Engenere Partner Edit Draft Restriction",
+    "summary": "Block partner edition when stage.state ≠ draft",
+    "version": "14.0.1.0.0",
+    "author": "Engenere",
+    "maintainers": ["felipempereira"],
+    "website": "https://engenere.one",
+    "license": "AGPL-3",
+    "category": "Contacts",
+    "depends": [
+        "partner_stage",
+    ],
+}
