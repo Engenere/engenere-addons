533b5d281a5f49105b84904d22fd9df45d84c03c
Felipe Motter <felipe@engenere.one>
2025-08-04 13:44:16 -0300

    [ADD] engenere_web_company_color_responsive: init module

---
+++ engenere_web_company_color_responsive/__manifest__.py
@@ -0,0 +1,15 @@
+# @author Felipe Motter Pereira <felipe@engenere.one>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Engenere Web Company Color For Responsive Client",
+    "summary": "Integration between Company Color And Responsive",
+    "version": "16.0.1.0.0",
+    "category": "Website",
+    "website": "https://github.com/Engenere/engenere-addons",
+    "author": "Engenere, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["web_responsive", "web_company_color"],
+    "auto_install": True,
+    "maintainers": ["felipemotter"],
+    "post_init_hook": "post_init_hook",
+}
---
+++ engenere_web_company_color_responsive/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+Integration between Company Color And Responsive
