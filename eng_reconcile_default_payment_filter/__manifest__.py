05a31651b773241ae75634f838b541c22d69a036
Felipe Motter <felipe@engenere.one>
2026-01-28 17:17:59 -0300

    [ADD] eng_reconcile_default_payment_filter: always enable payment filter
    Force the Outstanding Payments/Receipts filter to be active by default
    in bank reconciliation, regardless of whether a partner is set on the
    bank statement line.

---
+++ eng_reconcile_default_payment_filter/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Reconcile Default Payment Filter",
+    "summary": "Always enable Outstanding Payments/Receipts filter"
+    " in bank reconciliation.",
+    "license": "AGPL-3",
+    "author": "Engenere",
+    "website": "https://github.com/Engenere/engenere-addons",
+    "version": "16.0.1.0.0",
+    "depends": [
+        "account_reconcile_oca_add_default_filters",
+    ],
+    "data": [
+        "views/account_bank_statement_line.xml",
+    ],
+    "installable": True,
+}
